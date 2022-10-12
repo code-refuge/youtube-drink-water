61697ddbff266af6686c063386e99f3ebff4f51a
Joao Mantovani <jpm.mantovani45@gmail.com>
2022-10-12 16:37:49 -0300

    feat: first version

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider } from "native-base";
+import { StyleSheet, Text, View } from "react-native";
+import { Dashboard } from "./src/screens/Dashboard";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Dashboard />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/Dashboard.tsx
@@ -0,0 +1,88 @@
+import { Box, Button, HStack, Text, VStack } from 'native-base';
+import React, { useEffect, useState } from 'react';
+import { useToast } from 'native-base';
+
+interface IDashboardProps {
+
+}
+
+export const Dashboard: React.FC<IDashboardProps> = () => {
+  const [cupSize, setCupSize] = useState<number>(300);
+  const [water, setWater] = useState<number>(0);
+  const [goal, setGoal] = useState<number>(2000);
+
+  const toast = useToast();
+
+  const handleWater = () => {
+    setWater(water + cupSize);
+    toast.show({
+      description: `Você bebeu ${cupSize}ml de água`,
+    })
+  };
+
+  const handleChangeCupSize = (size: number) => {
+    setCupSize(size);
+  };
+
+  useEffect(() => {
+    if (water >= goal) {
+      toast.show({
+        description: "Você atingiu sua meta de água",
+        placement: "top",
+        colorScheme: "success",
+      })
+    }
+  }, [water]);
+
+  return (
+    <VStack
+      flex={1}
+      width='100%'
+      justifyContent='space-between'
+      alignItems='center'
+      p={4}
+      my={30}
+    >
+      <Text fontSize="sm">
+        {'  '} copo de {cupSize}ml
+      </Text>
+
+
+      <VStack>
+        <HStack alignItems="center" justifyContent="center">
+          <Text fontSize="6xl">
+            {water}
+          </Text>
+          <Text fontSize="xl">
+            {'  '}/ {goal} ml
+          </Text>
+        </HStack>
+
+
+
+        <Button
+          mt={5}
+          colorScheme="primary"
+          onPress={handleWater}
+        >
+          Beber água
+        </Button>
+      </VStack>
+
+      <Box mt={10}>
+        <Button.Group >
+          <Button onPress={() => handleChangeCupSize(200)} colorScheme="teal">
+            Copo americano
+          </Button>
+          <Button onPress={() => handleChangeCupSize(350)} colorScheme="teal">
+            Xicára
+          </Button>
+          <Button onPress={() => handleChangeCupSize(500)} colorScheme="teal">
+            Garrafa
+          </Button>
+        </Button.Group>
+      </Box>
+
+    </VStack>
+  );
+};
